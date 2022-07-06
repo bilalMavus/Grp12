d59a08db105cae235dd9a7dd28f58dc8c9653949
MARZİYEH <marziyeh.haydari59@gmail.com>
2022-07-06 22:50:51 +0300

    marziyeh yazdim3

--- src/marziyeh.java
+++ src/marziyeh.java
@@ -2,5 +2,6 @@ public class marziyeh {
     public static void main(String[] args) {
         System.out.println("merhaba marziyeh");
         System.out.println("marziyeh 2");
+        System.out.println("marziyeh 3");
     }
 }
