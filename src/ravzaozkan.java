e4e73923fc37f5373cc2d77883942cdad0ee7446
ravza <ravzabak@gmail.com>
2022-07-06 22:34:41 +0300

    yeni kod ravza1

--- src/ravzaozkan.java
+++ src/ravzaozkan.java
@@ -2,5 +2,8 @@ public class ravzaozkan {
 
     public static void main(String[] args) {
         System.out.println( "merhaba ravza");
+
+        System.out.println( "ravza bak ozkan");
+        System.out.println( " Muammer ozkan");
     }
 }
